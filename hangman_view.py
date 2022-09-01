7bd5fba9c6fcbf43538c7005c1394097e89c5a43
GeovaneSchmitz <contato@geovanems.com.br>
2022-09-01 17:33:56 -0300

    Cria interface de desenho do jogo

---
+++ hangman_view.py
@@ -0,0 +1,10 @@
+
+class HangmanView:
+    """
+        Class para desenho.
+    """
+    def __init__(self, word: str):
+        pass
+
+    def draw(self, used_letter: set[str], num_of_errors: int):
+        pass
