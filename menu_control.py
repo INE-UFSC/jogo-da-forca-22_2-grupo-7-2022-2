63eae620a99be0296fab1b618d592f15ad915a1f
anaard <anareginadelazzari@gmail.com>
2022-09-01 20:06:15 -0300

    mudança do show options

--- menu_control.py
+++ menu_control.py
@@ -3,14 +3,13 @@ from random import randint
 class MenuControl():
 
     def show_options(self) -> None:
-
-        state = input("Bem-vindo ao Jogo da forca! :D\nDigite o numero referente a opcao desejada:\n1: Modo normal\n2: Sorteio de palavras\n")
+        state = input("Bem-vindo ao Jogo da forca! :D\nDigite o número referente a opção desejada:\n1: Modo normal\n2: Sorteio de palavras\n").strip()
+        while state not in '12' or state == '12':
+            state = input("Opção não disponível.\nDigite um número válido referente a opção desejada:\n1: Modo normal\n2: Sorteio de palavras\n").strip()
         if state == '1':
             return self.direct_word()
         elif state == '2':
             return self.many_words()
-        else:
-            print("digite um valor valido.")
         #precisa de um retorno aqui heh
 
     def direct_word(self) -> str:
@@ -24,7 +23,7 @@ class MenuControl():
         word_list = []
         run = True
         word_index = 1
-        print('Neste modo, voce insere varias palavras e nos selecionamos uma para voce.')
+        print('Neste modo, você insere várias palavras e nós selecionamos uma para você.')
         print('Pressione enter sem digitar nada para enviar as palavras desejadas.')
         while run:
             print('Digite a palavra ', word_index)
