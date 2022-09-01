75d56cfc6f18489475753140da6880b82b5ed2a4
anaard <anareginadelazzari@gmail.com>
2022-09-01 18:51:55 -0300

    mudança das variáveis

--- word.py
+++ word.py
@@ -1,14 +1,14 @@
 class Word:
     def get_word(self):
         # Input da palavra a ser adivinhada e mudança para letras maiúsculas
-        self.palavra = input('Qual a palavra a ser adivinhada? ').strip().upper()
-        while not self.palavra.isalpha():
-            self.palavra = input('Digite apenas letras. Qual a palavra a ser adivinhada? ').strip().upper()
-        return self.palavra
+        self.word = input('Qual a palavra a ser adivinhada? ').strip().upper()
+        while not self.word.isalpha():
+            self.word = input('Digite apenas letras. Qual a palavra a ser adivinhada? ').strip().upper()
+        return self.word
 
     def get_letter(self):
         # Input do chute da letra e mudança para maiúscula
-        self.resposta = input('Qual a letra a ser posicionada? ').upper().strip()
-        while (len(self.resposta) != 1) or not self.resposta.isalpha():
-            self.resposta = input('Digite apenas uma letra. Qual a letra a ser posicionada? ').upper().strip()
-        return self.resposta
+        self.answer = input('Qual a letra a ser posicionada? ').upper().strip()
+        while (len(self.answer) != 1) or not self.answer.isalpha():
+            self.answer = input('Digite apenas uma letra. Qual a letra a ser posicionada? ').upper().strip()
+        return self.answer
