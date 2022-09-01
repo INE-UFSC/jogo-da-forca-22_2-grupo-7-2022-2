fa35bdd08a3a842489eeb520af064004df5d3f10
oasj <oasjatoba1@gmail.com>
2022-09-01 20:12:42 -0300

    Atualiação main file.

---
+++ main.py
@@ -0,0 +1,25 @@
+from menu_control import MenuControl
+from word import Word
+from hangman_view import HangmanView
+
+menu = MenuControl()
+word = menu.show_options()
+word_check = set(word)
+view = HangmanView(word)
+
+used_letters = set()
+num_of_errors = len(used_letters)
+num_max_errors = 7
+
+while True:
+    view.draw(used_letters)
+
+    letter = Word.get_letter()
+    used_letters.add(letter)
+
+    if used_letters >= word_check:
+        # Ganhou
+        pass
+    elif num_of_errors >= num_max_errors:
+        # Perdeu
+        pass
